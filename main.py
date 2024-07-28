a411c6e04717b7deeeaef35c442d1cba4cbc2101
shiamakd25 <shiamakdas@outlook.com>
2024-07-28 17:33:05 -0400

    Linked topics.csv file to output.pdf through pandas dataframe; output.pdf now displays topics from topics.csv as headers.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="", size=12)
-pdf.cell(w=0, h=12, txt="Hello There", align="L", ln=1, border=1)
-pdf.cell(w=0, h=12, txt="Hello There Again", align="L", ln=1, border=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(50,50,200)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
